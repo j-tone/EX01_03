5d9b645ec12c995835e7cfc91ce4f3866e99d6af
j-tone <jtoney17@my.whitworth.edu>
2014-05-21 14:17:57 -0700

    Round Two.
    Looks like it's working! I came back and it said "one uncommitted
    changes", and it was the addition of HWex1_4. Sweet!

--- HWEx1_3/HWEx1_3/Source.cpp
+++ HWEx1_3/HWEx1_3/Source.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 
 void ex03();
+void ex04();
+void Dubs(int &d);
+int Add(int rand1, int rand2);
+void AddByOne(int &y);
 
 int main()
 {
 	ex03();
+	ex04();
 }
 
 void ex03(){
@@ -43,4 +48,58 @@ void ex03(){
 
 	string empty = "I initialized the empty string";
 	cout << empty << endl;
+}
+
+void ex04()
+{
+	int p = 0;
+	cout << "Pleases to enter a numero in betwixt or equal to 1 and 10.\n";
+	cin >> p;
+	while ((p > 10) || (p < 1)){
+		cout << "Ah, no. Betwixt 1 and 10.\n";
+		cin >> p;
+	}
+
+	int sumofcubes = 0;
+	for (int i = 0; i < p; i++){
+		sumofcubes += (i*i)*i;
+	}
+	cout << "The sum of cubes from 1 to " << p << " equals " << sumofcubes << ".\n";
+
+	for (int a = 0; a < p; a++){
+		cout << "*";
+	}
+	cout << endl;
+
+	for (int e = 0; e <= 40; e += 2){
+		cout << e << " ";
+	}
+	cout << endl;
+
+	Dubs(p);
+	cout << "\nSo I've doubled that one number betwixt 1 and 10 that you gave me earlier.\n";
+	cout << "Here it is: " << p << endl << endl;
+
+	cout << "This is the sum of two random numbers: " << Add(rand(), rand()) << endl;
+	
+	cout << "\nLet's take the number we got earlier and was doubled.\n";
+	cout << "That's this number: " << p << endl;
+	cout << "And now I just added one to it.\n";
+	AddByOne(p);
+	cout << p << endl;
+}
+
+void Dubs(int &d)
+{
+	d += d;
+}
+
+int Add(int rand1, int rand2)
+{
+	return (rand1 + rand2);
+}
+
+void AddByOne(int &y)
+{
+	y++;
 }
