c211be616bc391ed1b22b36f5e721105e804a360
j-tone <jtoney17@my.whitworth.edu>
2014-05-21 23:33:58 -0700

    So this is it. It's all there.

--- HWEx1_3/HWEx1_3/Source.cpp
+++ HWEx1_3/HWEx1_3/Source.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
+void ex02();
+
 void ex03();
+
 void ex04();
 void Dubs(int &d);
 int Add(int rand1, int rand2);
 void AddByOne(int &y);
 
+void ex05();
+void arrayCount(int set[], int size);
+void arrayAsk(int set[], int size);
+
 int main()
 {
+	srand(time(NULL));
+	ex02();
 	ex03();
 	ex04();
+	ex05();
+}
+
+void ex02()
+{
+	bool hasPassedTest = true;
+	int x = rand(), y = rand();
+	cout << x << " " << y << endl;
+	if (x >= y)
+		cout << "X is, in fact, greater than or equal to Y.\n";
+	else
+		cout << "Nah man, X ain't greater than or equal to Y.\n";
+
+	int numberOfShares;
+	cout << "Enter a value for 'numberOfShares': ";
+	cin >> numberOfShares;
+	cout << ((numberOfShares < 100) ? "It's under 100!\n" : "It's over/equal to 100!\n");
+
+	int bx, bk;
+	cout << "Now, enter a box width, and then a book width: ";
+	cin >> bx;
+	cin >> bk;
+	if (bx % bk == 0)
+		cout << "So, you can fit " << (bx / bk) << " books evenly along the box's width.\n";
+	else
+		cout << "You're going to have some space not big enough for a book left over.\n";
+
+	int sl, temp;
+	cout << "Now, enter the shelf life of a box of chocolate,\nand then enter the current temperature outside: ";
+	cin >> sl;
+	cin >> temp;
+	if (temp > 90)
+		cout << "The new shelf live is now " << sl - 4 << ".\n";
+	else
+		cout << "The shelf life is still " << sl << ".\n";
 }
 
 void ex03(){
@@ -102,4 +147,51 @@ int Add(int rand1, int rand2)
 void AddByOne(int &y)
 {
 	y++;
+}
+
+void ex05()
+{
+	int numbers[5];
+	for (int i = 0; i < 5; i++){
+		cout << "Enter an INTEGER please: ";
+		cin >> numbers[i];
+	}
+
+	int product = 1, sum = 0;
+	for (int i = 0; i < 5; i++){
+		product *= numbers[i];
+		sum += numbers[i];
+	}
+	cout << "\nThe product of those numbers is: " << product;
+	cout << "\nThe sum of those numbers is: " << sum << endl;
+
+	arrayCount(numbers, 5);
+	cout << endl;
+
+	arrayAsk(numbers, 5);
+}
+
+void arrayCount(int set[], int size)
+{
+	cout << "\nThese are the numbers in the array:\n";
+	for (int i = 0; i < size; i++){
+		cout << set[i] << " ";
+	}
+}
+
+void arrayAsk(int set[], int size)
+{
+	int p;
+	bool value = false;
+	cout << "Enter a value, maybe it'll be in the array: ";
+	cin >> p;
+	for (int i = 0; i < size; i++){
+		if (p == set[i]){
+			value = true;
+		}
+	}
+	if (value == true)
+		cout << "Indeed, you have chosen a value that is inside the array.\n";
+	else
+		cout << "You didn't pick a value inside the array.\n";
 }
